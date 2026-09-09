4620935c55f2c66fa827819e1ed173993aa22ff4
Angelicadelgadonuno <angelicadelgadonuno@gmail.com>
2026-09-09 11:49:22 +0200

    feat: add play method

--- src/main/java/Nivel1/InstrumentosMusicales/PercussionInstrument.java
+++ src/main/java/Nivel1/InstrumentosMusicales/PercussionInstrument.java
@@ -5,5 +5,8 @@ public class PercussionInstrument extends Instrument {
     public PercussionInstrument(String name, int price) {
         super(name, price);
     }
-
+    @Override
+    public void play (){
+        System.out.println("A Percussion Instrument is playing");
+    }
 }
