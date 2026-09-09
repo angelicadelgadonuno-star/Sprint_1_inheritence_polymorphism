1659f432479963b244196d3cc240456de8c23b87
Angelicadelgadonuno <angelicadelgadonuno@gmail.com>
2026-09-09 11:44:15 +0200

    feat: add play method

--- src/main/java/Nivel1/InstrumentosMusicales/WindInstrument.java
+++ src/main/java/Nivel1/InstrumentosMusicales/WindInstrument.java
@@ -1,4 +1,15 @@
 package Nivel1.InstrumentosMusicales;
 
 public class WindInstrument extends Instrument {
+
+
+    public WindInstrument(String name, int price) {
+        super(name, price);
+    }
+
+    @Override
+    public void play (){
+        System.out.println("A Wind Instrument is playing");
+    }
+
 }
