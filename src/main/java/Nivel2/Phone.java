08e6f5d21e41bde8ec8449796cd60da9e793d79b
Angelicadelgadonuno <angelicadelgadonuno@gmail.com>
2026-09-10 15:51:07 +0200

    feat: create call method

--- src/main/java/Nivel2/Phone.java
+++ src/main/java/Nivel2/Phone.java
@@ -5,9 +5,15 @@ public class Phone {
     private String brand;
     private String model;
 
+
     public Phone(String brand, String model) {
         this.brand = brand;
         this.model = model;
+    }
+
+    public void call(int number){
+       System.out.println("You´re calling the following number: " + number);
+
     }
 
 
