57e84230911ebaef0130990e59e50132a86c274e
Angelicadelgadonuno <angelicadelgadonuno@gmail.com>
2026-09-10 12:55:51 +0200

    feat: updated attributes and constructor

--- src/main/java/Nivel1/ClasseCotxe/Main.java
+++ src/main/java/Nivel1/ClasseCotxe/Main.java
@@ -5,9 +5,14 @@ public class Main {
 
         Car.brake();
 
-        Car car = new Car ( "Volvo", "EX360", "680" );
+        Car car = new Car ( "Volvo", "EX360", 680 );
 
         car.accelerate();
 
+        System.out.println(Car.brake());
+
+        System.out.println(Car.brand);
+        System.out.println(Car.model);
+        System.out.println(Car.power);
     }
 }
